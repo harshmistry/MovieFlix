d8efe048723a6eecb20f99a3b514edf7fadaece4
Harsh Mistry <harshmistry@users.noreply.github.com>
2016-08-28 19:04:58 -0400

    Update DBFinalVariables.java
    Removed password

--- src/main/java/io/egen/movieflix/constants/DBFinalVariables.java
+++ src/main/java/io/egen/movieflix/constants/DBFinalVariables.java
@@ -4,7 +4,7 @@ public final class DBFinalVariables {
 	
 	//db settings
 	public static final String DB_USERNAME 			=	"root";
-	public static final String DB_PASSWORD			=	"harsh";
+	public static final String DB_PASSWORD			=	"";
 	public static final String DB_URL				=	"jdbc:mysql://localhost:3306/egen-db-movieflix?useSSL=false";
 	public static final String DB_DRIVER_CLASSNAME	=	"com.mysql.jdbc.Driver";
 	
